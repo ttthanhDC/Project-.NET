dd97899a870dc248312263877aca1201c0357b86
conghung <conghung.hut@gmail.com>
2017-07-29 15:39:38 +0700

    update

---
+++ SYSGREEN/DataObject/Shiper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataObject
+{
+    class Shiper
+    {
+        public string Table = "SHIPER";
+        public int SHIPER_ID { get; set; }
+        public String NUMBER { get; set; }
+        public String NAME { get; set; }
+        public int NGAYHOADON_ID { get; set; }
+        public String DESCRIPTION { get; set; }
+       
+    }
+}
