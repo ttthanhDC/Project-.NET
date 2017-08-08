b449be974594ea101e062b12474d7b870b6ebfef
ttthanhDC <thanhdc241189@gmail.com>
2017-08-08 22:07:41 +0700

    1

--- SYSGREEN/Servies/HoaDonServices.cs
+++ SYSGREEN/Servies/HoaDonServices.cs
@@ -427,7 +427,7 @@ namespace Servies
             DataTable table = new DataTable();
             SqlCommand cmd = null;
             SqlConnection conn = Common.Connection.SqlConnect();
-            String Select = "Select * from vHoaDonShiper ORDER BY ID_CTHD ";
+            String Select = "Select * from vHoaDonShiper ORDER BY IdLotrinhShipper ";
 
             cmd = new SqlCommand(Select);
             cmd.CommandType = CommandType.Text;
@@ -443,7 +443,7 @@ namespace Servies
             DataTable table = new DataTable();
             SqlCommand cmd = null;
             SqlConnection conn = Common.Connection.SqlConnect();
-            String Select = "Select * from vHoaDonShiper where ID_NHD in (" + lstID + ") ORDER BY ID_CTHD ";
+            String Select = "Select * from vHoaDonShiper where ID_NHD in (" + lstID + ") ORDER BY IdLotrinhShipper ";
             cmd = new SqlCommand(Select);
             cmd.CommandType = CommandType.Text;
             cmd.Connection = conn;
@@ -458,7 +458,22 @@ namespace Servies
             DataTable table = new DataTable();
             SqlCommand cmd = null;
             SqlConnection conn = Common.Connection.SqlConnect();
-            String Select = "Select * from vHoaDonShiper where IdLotrinhShipper = " + lstID + " ORDER BY ID_CTHD";
+            String Select = "Select * from vHoaDonShiper where IdLotrinhShipper = " + lstID + " ORDER BY IdLotrinhShipper";
+            cmd = new SqlCommand(Select);
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = conn;
+            conn.Open();
+            table.Load(cmd.ExecuteReader());
+            conn.Close();
+            return table;
+        }
+
+        public static DataTable getAllDataViewHoaDonByMaLoTrinh(String MaLotrinh)
+        {
+            DataTable table = new DataTable();
+            SqlCommand cmd = null;
+            SqlConnection conn = Common.Connection.SqlConnect();
+            String Select = "Select * from vHoaDonShiper where MaLoTrinh  LIKE N'%" + MaLotrinh + "%' ORDER BY IdLotrinhShipper";
             cmd = new SqlCommand(Select);
             cmd.CommandType = CommandType.Text;
             cmd.Connection = conn;
@@ -503,7 +518,7 @@ namespace Servies
             {
                 Select += "TrangThaiHD LIKE N'%" + trangThai + "%' AND ";
             }
-            Select += "1=1 ORDER BY ID_CTHD";
+            Select += "1=1 ORDER BY IdLotrinhShipper";
             cmd = new SqlCommand(Select);
             cmd.CommandType = CommandType.Text;
             cmd.Connection = conn;
