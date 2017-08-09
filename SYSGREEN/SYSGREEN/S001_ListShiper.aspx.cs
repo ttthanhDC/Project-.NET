5652a0f708d74ebdcfc00d6fd2514ccc8d240b8d
thanhdc241189@gmail.com <thanhdc241189@gmail.com>
2017-08-09 18:58:59 +0700

    1

--- SYSGREEN/SYSGREEN/S001_ListShiper.aspx.cs
+++ SYSGREEN/SYSGREEN/S001_ListShiper.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +14,26 @@ namespace SYSGREEN
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //String NgayHD = context.Request.Form["NgayHD"].ToString();
+            DataTable table = new DataTable();
+            SqlConnection conn = Common.Connection.SqlConnect();
+            conn.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("PGetStepV3", conn);
+            sda.SelectCommand.CommandType = CommandType.StoredProcedure;
+            sda.SelectCommand.Parameters.AddWithValue("@NgayHD", "08/08/2017");
+            DataSet ds = new DataSet();
+            sda.Fill(ds, "vHoaDonStep3");
+            conn.Close();
+            CrystalDecisions.CrystalReports.Engine.ReportDocument rpt = new CrystalDecisions.CrystalReports.Engine.ReportDocument();
+            rpt.Load(System.Web.HttpContext.Current.Server.MapPath("~/hoaDon.rpt"));
+            rpt.SetDataSource(ds);
+            CrystalReportViewer1.ReportSource = rpt;
+            CrystalReportViewer1.Visible = true;
+            CrystalReportViewer1.RefreshReport();
         }
     }
 }
--- SYSGREEN/SYSGREEN/S001_ListShiper.aspx.designer.cs
+++ SYSGREEN/SYSGREEN/S001_ListShiper.aspx.designer.cs
@@ -11,5 +11,23 @@ namespace SYSGREEN {
     
     
     public partial class S001_ListShiper {
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// CrystalReportViewer1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::CrystalDecisions.Web.CrystalReportViewer CrystalReportViewer1;
     }
 }
